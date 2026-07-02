718e1af9d25ba211e6afe54da460652a9a522959
turtton <top.gear7509@turtton.net>
2026-07-02 09:43:34 +0900

    feat(nextcloud): expose via Cloudflare Tunnel with Access protection

--- terraform/tunnel.tf
+++ terraform/tunnel.tf
@@ -4,16 +4,17 @@ locals {
   domain      = "turtton.net"
 
   tunnel_ingress = {
-    grafana  = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local:80"
-    longhorn = "http://longhorn-frontend.longhorn-system.svc.cluster.local:80"
-    livesync = "http://couchdb.obsidian-livesync.svc.cluster.local:5984"
-    kameuo   = "http://iceshrimp-web.iceshrimp.svc.cluster.local:3000"
+    grafana    = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local:80"
+    longhorn   = "http://longhorn-frontend.longhorn-system.svc.cluster.local:80"
+    livesync   = "http://couchdb.obsidian-livesync.svc.cluster.local:5984"
+    kameuo     = "http://iceshrimp-web.iceshrimp.svc.cluster.local:3000"
     forgejo    = "http://forgejo-http.forgejo.svc.cluster.local:3000"
     woodpecker = "http://woodpecker-server.woodpecker.svc.cluster.local:80"
     chat       = "http://caddy.fluxer.svc.cluster.local:80"
+    nextcloud  = "http://nextcloud.nextcloud.svc.cluster.local:8080"
   }
 
-  access_protected_hostnames = ["grafana", "longhorn"]
+  access_protected_hostnames = ["grafana", "longhorn", "nextcloud"]
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "homelab" {
